372fbe373836ab5f8b9a734e76e485c56c0ba919
uzairnazir2002 <uzairnazir42@gmail.com>
2026-03-31 00:53:38 +0500

    responsiveness-fixes

---
+++ js/content.js
@@ -0,0 +1,105 @@
+window.birthdayContent = {
+  pageTitle: "Happy Birthday",
+  metaDescription: "A birthday celebration page with music, balloons, and photo memories.",
+  ogDescription: "Wish you a very happy birthday",
+  intro: {
+    eyebrow: "A little celebration made just for you",
+    title: "Happy Birthday,\nBabyy",
+    subtitle: "Soft lights, warm music, floating balloons, and the sweetest memories all in one place."
+  },
+  centerpiece: {
+    kicker: "Tap through the buttons below",
+    title: "The surprise begins here."
+  },
+  audioSrc: "assets/audio/hbd.mp3",
+  profilePhoto: {
+    src: "assets/images/person.png",
+    alt: "Birthday portrait"
+  },
+  photos: [
+    {
+      src: "assets/images/photo1.jpg",
+      alt: "Birthday memory one"
+    },
+    {
+      src: "assets/images/photo2.jpg",
+      alt: "Birthday memory two"
+    },
+    {
+      src: "assets/images/photo3.jpg",
+      alt: "Birthday memory three"
+    },
+    {
+      src: "assets/images/photo4.jpg",
+      alt: "Birthday memory four"
+    }
+  ],
+  buttons: {
+    turnOn: "Turn On The Lights",
+    play: "Start The Music",
+    banner: "Bring In The Banner",
+    balloons: "Release The Balloons",
+    cake: "Show The Cake",
+    candle: "Light The Candle",
+    wish: "Make A Wish",
+    story: "Read The Message"
+  },
+  balloons: [
+    { letter: "H", color: "#F2B300" },
+    { letter: "B", color: "#0719D4" },
+    { letter: "D", color: "#D14D39" },
+    { letter: "B", color: "#8FAD00" },
+    { letter: "A", color: "#8377E4" },
+    { letter: "B", color: "#99C96A" },
+    { letter: "Y", color: "#20CFB4" }
+  ],
+  message: [
+    "Today is...",
+    "as beautiful as other days",
+    "but you realize",
+    "another year has gone",
+    "in a blink of the eyes",
+    "however",
+    "Do you know..?",
+    "today is just special",
+    "so special to you",
+    "that's why",
+    "Let's make it...",
+    "the best celebration ever",
+    "and let me share...",
+    "a piece of happiness to you",
+    "I made all this...",
+    "as a birthday present to you",
+    "I'm glad we met on zzz",
+    "thanks for the friendship we made",
+    "thanks for everything",
+    "I wish you all the best",
+    "May your life be at ease",
+    "May all your wishes come true",
+    "Remember",
+    "your ambitions",
+    "you live as a free bird...",
+    "flying in the blue sky",
+    "Now things are different...",
+    "real story of your life",
+    "is just about to begin",
+    "indeed..",
+    "but...",
+    "don't worry",
+    "because...",
+    "God has your back",
+    "and",
+    "this year will be better",
+    "and I hope",
+    "you'll find...",
+    "happiness along the way",
+    "keep your spirit up",
+    "enjoy every single moment...",
+    "that you experience today",
+    "fill it with your most beautiful smile",
+    "and make it the best memory..",
+    "lastly...",
+    "I'd like to wish you one more time",
+    "a very happy birthday babyy \u2764"
+  ]
+};
--- js/effect.js
+++ js/effect.js
@@ -1,227 +1,344 @@
-$(window).load(function(){
-	$('.loading').fadeOut('fast');
-	$('.container').fadeIn('fast');
-});
-$('document').ready(function(){
-		var vw;
-		$(window).resize(function(){
-			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
-		});
-
-	$('#turn_on').click(function(){
-		$('#bulb_yellow').addClass('bulb-glow-yellow');
-		$('#bulb_red').addClass('bulb-glow-red');
-		$('#bulb_blue').addClass('bulb-glow-blue');
-		$('#bulb_green').addClass('bulb-glow-green');
-		$('#bulb_pink').addClass('bulb-glow-pink');
-		$('#bulb_orange').addClass('bulb-glow-orange');
-		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#play').fadeIn('slow');
-		});
-	});
-	$('#play').click(function(){
-		var audio = $('.song')[0];
-        audio.play();
-        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
-		$('#bulb_red').addClass('bulb-glow-red-after');
-		$('#bulb_blue').addClass('bulb-glow-blue-after');
-		$('#bulb_green').addClass('bulb-glow-green-after');
-		$('#bulb_pink').addClass('bulb-glow-pink-after');
-		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('backgroud-color','#FFF');
-		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#bannar_coming').fadeIn('slow');
-		});
-	});
-
-	$('#bannar_coming').click(function(){
-		$('.bannar').addClass('bannar-come');
-
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#balloons_flying').fadeIn('slow');
-
-		// Show the album photos
-		$('.album-photo').fadeIn('slow');
-
-		$('.can-zoom').fadeIn('slow');
-
-		});
-	});
-
-	function loopOne() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
-			loopOne();
-		});
-	}
-	function loopTwo() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b2').animate({left:randleft,bottom:randtop},10000,function(){
-			loopTwo();
-		});
-	}
-	function loopThree() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b3').animate({left:randleft,bottom:randtop},10000,function(){
-			loopThree();
-		});
-	}
-	function loopFour() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b4').animate({left:randleft,bottom:randtop},10000,function(){
-			loopFour();
-		});
-	}
-	function loopFive() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b5').animate({left:randleft,bottom:randtop},10000,function(){
-			loopFive();
-		});
-	}
-
-	function loopSix() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSix();
-		});
-	}
-	function loopSeven() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSeven();
-		});
-	}
-	function loopEight() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSeven();
-		});
-	}
-
-	$('#balloons_flying').click(function(){
-		$('.balloon-border').animate({top:-500},8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6',).addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
-		loopOne();
-		loopTwo();
-		loopThree();
-		loopFour();
-		loopFive();
-		loopSix();
-		loopSeven();
-		// loopEight();
-
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#cake_fadein').fadeIn('slow');
-		});
-	});	
-
-	$('#cake_fadein').click(function(){
-		$('.cake').fadeIn('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#light_candle').fadeIn('slow');
-		});
-	});
-
-	$('#light_candle').click(function(){
-		$('.fuego').fadeIn('slow');
-		$(this).fadeOut('slow').promise().done(function(){
-			$('#wish_message').fadeIn('slow');
-		});
-	});
-
-		
-	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
-
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22')
-		$('#b3').attr('id','b33')
-		$('#b4').attr('id','b44')
-		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
-		$('.balloons').css('opacity','0.9');
-		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#story').fadeIn('slow');
-		});
-	});
-	
-	$('#story').click(function(){
-		$(this).fadeOut('slow');
-		$('.cake').fadeOut('fast').promise().done(function(){
-			$('.message').fadeIn('slow');
-		});
-
-		var $messages = $(".message p");   // only inside .message
-		var totalMessages = $messages.length;
-
-		function msgLoop(i) {
-			if (i < totalMessages - 1) {
-				$messages.eq(i).fadeIn('slow').delay(1500).fadeOut('slow').promise().done(function(){
-					msgLoop(i + 1);
-				});
-			} else {
-				// Last message stays + cake comes back
-				$messages.eq(i).fadeIn('slow').promise().done(function(){
-					$('.cake').fadeIn('fast');
-				});
-			}
-		}
-
-		msgLoop(0);
-	});
-
+$(window).on("load", function () {
+  $(".loading").fadeOut("fast");
+  $(".container").fadeIn("fast");
 });
 
-// Zoom (lightbox) feature
-$('.album-photo').click(function() {
-    var src = $(this).attr('src');
-    $('#lightbox img').attr('src', src);
+$(function () {
+  var content = window.birthdayContent || {};
+  var balloonSelectors = ["#b1", "#b2", "#b3", "#b4", "#b5", "#b6", "#b7"];
+  var balloonsAligned = false;
+  var balloonLoopsStarted = false;
 
-    // Force flex only when showing
-    $('#lightbox').css('display', 'flex').hide().fadeIn('fast');
+  function escapeHtml(value) {
+    return String(value)
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#39;");
+  }
+
+  function renderMultilineText(value) {
+    return escapeHtml(value).replace(/\n/g, "<br>");
+  }
+
+  function applyContent() {
+    if (content.pageTitle) {
+      document.title = content.pageTitle;
+    }
+
+    if (content.metaDescription) {
+      $("#meta-description").attr("content", content.metaDescription);
+    }
+
+    if (content.ogDescription) {
+      $("#meta-og-description").attr("content", content.ogDescription);
+    }
+
+    if (content.intro) {
+      $("#intro-eyebrow").text(content.intro.eyebrow || "");
+      $("#intro-title").html(renderMultilineText(content.intro.title || ""));
+      $("#intro-subtitle").text(content.intro.subtitle || "");
+    }
+
+    if (content.centerpiece) {
+      $("#centerpiece-kicker").text(content.centerpiece.kicker || "");
+      $("#centerpiece-title").text(content.centerpiece.title || "");
+    }
+
+    if (content.audioSrc) {
+      $("#song-source").attr("src", content.audioSrc);
+      $(".song").get(0).load();
+    }
+
+    if (content.profilePhoto) {
+      $("#profile-photo").attr({
+        src: content.profilePhoto.src || $("#profile-photo").attr("src"),
+        alt: content.profilePhoto.alt || $("#profile-photo").attr("alt")
+      });
+    }
+
+    if (Array.isArray(content.photos)) {
+      content.photos.slice(0, 4).forEach(function (photo, index) {
+        $("#photo-" + (index + 1)).attr({
+          src: photo.src || $("#photo-" + (index + 1)).attr("src"),
+          alt: photo.alt || $("#photo-" + (index + 1)).attr("alt")
+        });
+      });
+    }
+
+    if (content.buttons) {
+      $("#turn_on").text(content.buttons.turnOn || $("#turn_on").text());
+      $("#play").text(content.buttons.play || $("#play").text());
+      $("#bannar_coming").text(content.buttons.banner || $("#bannar_coming").text());
+      $("#balloons_flying").text(content.buttons.balloons || $("#balloons_flying").text());
+      $("#cake_fadein").text(content.buttons.cake || $("#cake_fadein").text());
+      $("#light_candle").text(content.buttons.candle || $("#light_candle").text());
+      $("#wish_message").text(content.buttons.wish || $("#wish_message").text());
+      $("#story").text(content.buttons.story || $("#story").text());
+    }
+
+    if (Array.isArray(content.balloons)) {
+      content.balloons.slice(0, balloonSelectors.length).forEach(function (balloon, index) {
+        $(balloonSelectors[index] + " h2").text(balloon.letter || "").css("color", balloon.color || "");
+      });
+    }
+
+    if (Array.isArray(content.message) && content.message.length) {
+      $(".message").html(
+        content.message
+          .map(function (line) {
+            return "<p>" + escapeHtml(line) + "</p>";
+          })
+          .join("")
+      );
+    }
+  }
+
+  function viewportBounds() {
+    var width = $(window).width();
+    var height = $(window).height();
+    var padding = Math.max(20, width * 0.04);
+
+    return {
+      width: width,
+      height: height,
+      padding: padding,
+      minTop: Math.max(80, height * 0.12),
+      maxTop: Math.max(180, height * 0.62)
+    };
+  }
+
+  function seedBalloons() {
+    balloonSelectors.forEach(function (selector, index) {
+      $(selector).css({
+        left: 12 + (index * 12) + "vw",
+        top: "110vh"
+      });
+    });
+  }
+
+  function randomBalloonPosition(selector) {
+    var bounds = viewportBounds();
+    var $balloon = $(selector);
+    var balloonWidth = $balloon.outerWidth() || 80;
+    var maxLeft = Math.max(bounds.width - balloonWidth - bounds.padding, bounds.padding);
+    var nextLeft = bounds.padding + (Math.random() * Math.max(maxLeft - bounds.padding, 1));
+    var nextTop = bounds.minTop + (Math.random() * Math.max(bounds.maxTop - bounds.minTop, 1));
+
+    $balloon.animate(
+      {
+        left: nextLeft,
+        top: nextTop
+      },
+      10000,
+      "linear",
+      function () {
+        if (!balloonsAligned) {
+          randomBalloonPosition(selector);
+        }
+      }
+    );
+  }
+
+  function startBalloonLoops() {
+    if (balloonLoopsStarted) {
+      return;
+    }
+
+    balloonLoopsStarted = true;
+
+    balloonSelectors.forEach(function (selector, index) {
+      setTimeout(function () {
+        randomBalloonPosition(selector);
+      }, index * 180);
+    });
+  }
+
+  function alignBalloons() {
+    var bounds = viewportBounds();
+    var count = balloonSelectors.length;
+    var balloonWidth = $(balloonSelectors[0]).outerWidth() || 80;
+    var availableWidth = bounds.width - (bounds.padding * 2);
+    var step = availableWidth / count;
+    var rowTop = Math.min(Math.max(96, bounds.height * 0.16), 180);
+
+    balloonSelectors.forEach(function (selector, index) {
+      var nextLeft = bounds.padding + (step * index) + ((step - balloonWidth) / 2);
+
+      $(selector).stop(true, false).animate(
+        {
+          left: Math.max(bounds.padding, nextLeft),
+          top: rowTop
+        },
+        700
+      );
+    });
+  }
+
+  function closeLightbox() {
+    $("#lightbox").fadeOut("fast", function () {
+      $(this).attr("aria-hidden", "true");
+    });
+    $("body").removeClass("lightbox-open");
+  }
+
+  applyContent();
+  seedBalloons();
+
+  $(window).on("resize", function () {
+    if (balloonsAligned) {
+      alignBalloons();
+    }
+  });
+
+  $("#turn_on").on("click", function () {
+    $("#bulb_yellow").addClass("bulb-glow-yellow");
+    $("#bulb_red").addClass("bulb-glow-red");
+    $("#bulb_blue").addClass("bulb-glow-blue");
+    $("#bulb_green").addClass("bulb-glow-green");
+    $("#bulb_pink").addClass("bulb-glow-pink");
+    $("#bulb_orange").addClass("bulb-glow-orange");
+    $("body").addClass("peach");
+
+    $(this).fadeOut("slow").delay(1800).promise().done(function () {
+      $("#play").fadeIn("slow");
+    });
+  });
+
+  $("#play").on("click", function () {
+    var audio = $(".song")[0];
+
+    if (audio) {
+      audio.play();
+    }
+
+    $("#bulb_yellow").addClass("bulb-glow-yellow-after");
+    $("#bulb_red").addClass("bulb-glow-red-after");
+    $("#bulb_blue").addClass("bulb-glow-blue-after");
+    $("#bulb_green").addClass("bulb-glow-green-after");
+    $("#bulb_pink").addClass("bulb-glow-pink-after");
+    $("#bulb_orange").addClass("bulb-glow-orange-after");
+    $("body").addClass("peach-after");
+
+    $(this).fadeOut("slow").delay(1800).promise().done(function () {
+      $("#bannar_coming").fadeIn("slow");
+    });
+  });
+
+  $("#bannar_coming").on("click", function () {
+    $(".bannar").addClass("bannar-come");
+
+    $(this).fadeOut("slow").delay(1200).promise().done(function () {
+      $(".album-photo").fadeIn("slow");
+      $(".can-zoom").fadeIn("slow");
+      $("#balloons_flying").fadeIn("slow");
+    });
+  });
+
+  $("#balloons_flying").on("click", function () {
+    var borderTarget = -Math.max(160, $(window).width() * 0.15);
+
+    $(".balloon-border").animate({ top: borderTarget }, 6000);
+    $("#b1, #b4, #b5, #b7").addClass("balloons-rotate-behaviour-one");
+    $("#b2, #b3, #b6").addClass("balloons-rotate-behaviour-two");
+
+    balloonsAligned = false;
+    startBalloonLoops();
+
+    $(this).fadeOut("slow").delay(1600).promise().done(function () {
+      $("#cake_fadein").fadeIn("slow");
+    });
+  });
+
+  $("#cake_fadein").on("click", function () {
+    $(".cake").fadeIn("slow");
+
+    $(this).fadeOut("slow").delay(1400).promise().done(function () {
+      $("#light_candle").fadeIn("slow");
+    });
+  });
+
+  $("#light_candle").on("click", function () {
+    $(".fuego").fadeIn("slow");
+
+    $(this).fadeOut("slow").delay(800).promise().done(function () {
+      $("#wish_message").fadeIn("slow");
+    });
+  });
+
+  $("#wish_message").on("click", function () {
+    balloonsAligned = true;
+    alignBalloons();
+    $(".balloons").css("opacity", "0.94");
+    $(".balloons h2").fadeIn(2200);
+
+    $(this).fadeOut("slow").delay(1600).promise().done(function () {
+      $("#story").fadeIn("slow");
+    });
+  });
+
+  $("#story").on("click", function () {
+    var $messages = $(".message p");
+    var totalMessages = $messages.length;
+
+    $(this).fadeOut("slow");
+    $messages.hide();
+
+    $(".cake").fadeOut("fast").promise().done(function () {
+      $(".message").fadeIn("slow", function () {
+        var centerpiece = $(".centerpiece").get(0);
+
+        if (centerpiece && typeof centerpiece.scrollIntoView === "function") {
+          centerpiece.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+      });
+    });
+
+    function messageLoop(index) {
+      if (index < totalMessages - 1) {
+        $messages.eq(index).fadeIn("slow").delay(1500).fadeOut("slow").promise().done(function () {
+          messageLoop(index + 1);
+        });
+      } else {
+        $messages.eq(index).fadeIn("slow").promise().done(function () {
+          $(".cake").fadeIn("fast");
+        });
+      }
+    }
+
+    messageLoop(0);
+  });
+
+  $(".album-photo").on("click", function () {
+    var src = $(this).attr("src");
+    var alt = $(this).attr("alt") || "Zoomed birthday photo";
+
+    $("#lightbox img").attr({
+      src: src,
+      alt: alt
+    });
+
+    $("#lightbox")
+      .attr("aria-hidden", "false")
+      .css("display", "flex")
+      .hide()
+      .fadeIn("fast");
+
+    $("body").addClass("lightbox-open");
+  });
+
+  $("#lightbox").on("click", function (event) {
+    if (event.target !== this) {
+      return;
+    }
+
+    closeLightbox();
+  });
+
+  $(document).on("keyup", function (event) {
+    if (event.key === "Escape") {
+      closeLightbox();
+    }
+  });
 });
-
-// Close when clicking outside image
-$('#lightbox').click(function(e) {
-    if (e.target !== this) return; // only close if background clicked
-    $('#lightbox').fadeOut('fast');
-});
-
-
-
-
-//alert('hello');
